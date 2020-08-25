f55c55caa72645d027edb4305f47521f953e58ea
Boris P <pustovetov.boris@yandex.ru>
2020-08-26 02:01:14 +0300

    step 5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,52 +7,52 @@ const fixturesPath = path.resolve('./__fixtures__');
 
 // JSON test
 describe('JSON test group', () => {
-  const firstFile = 'file1.json';
-  const secondFile = 'file2.json';
-  const resultFile = 'resultJSON';
-  const [firstFilePath, secondFilePath, expectedFilePath, firstFileFormat,
-    secondFileFormat] = factory(fixturesPath, firstFile, secondFile, resultFile);
-  const expectedContent = fs.readFileSync(expectedFilePath, 'utf-8');
+  const firstJsonFile = 'file1.json';
+  const secondJsonFile = 'file2.json';
+  const resultJsonFile = 'resultJSON';
+  const [firstJsonFilePath, secondJsonFilePath, expectedJsonFilePath, firstJsonFileFormat,
+    secondJsonFileFormat] = factory(fixturesPath, firstJsonFile, secondJsonFile, resultJsonFile);
+  const expectedContent = fs.readFileSync(expectedJsonFilePath, 'utf-8');
 
   test('Files format should be JSON and both file formats should be equal', () => {
-    expect(firstFileFormat).toBe('.json');
-    expect(secondFileFormat).toBe('.json');
-    expect(firstFileFormat === secondFileFormat).toBeTruthy();
+    expect(firstJsonFileFormat).toBe('.json');
+    expect(secondJsonFileFormat).toBe('.json');
+    expect(firstJsonFileFormat === secondJsonFileFormat).toBeTruthy();
   });
 
   test('Should return difference between two JSON files', () => {
-    expect(genDiff(firstFilePath, secondFilePath)).toBe(expectedContent);
+    expect(genDiff(firstJsonFilePath, secondJsonFilePath)).toBe(expectedContent);
   });
 
   test('Result file shoud be truthy and defined', () => {
-    expect(genDiff(firstFilePath, secondFilePath)).toBeTruthy();
-    expect(genDiff(firstFilePath, secondFilePath)).toBeDefined();
+    expect(genDiff(firstJsonFilePath, secondJsonFilePath)).toBeTruthy();
+    expect(genDiff(firstJsonFilePath, secondJsonFilePath)).toBeDefined();
   });
 });
 //
 
 // YAML test
 describe('YAML test group', () => {
-  const firstFile = 'file1.yaml';
-  const secondFile = 'file2.yaml';
-  const resultFile = 'resultYAML';
-  const [firstFilePath, secondFilePath, expectedFilePath, firstFileFormat,
-    secondFileFormat] = factory(fixturesPath, firstFile, secondFile, resultFile);
-  const expectedContent = fs.readFileSync(expectedFilePath, 'utf-8');
+  const firstYamlFile = 'file1.yaml';
+  const secondYamlFile = 'file2.yaml';
+  const resultYamlFile = 'resultYAML';
+  const [firstYamlFilePath, secondYamlFilePath, expectedYamlFilePath, firstYamlFileFormat,
+    secondYamlFileFormat] = factory(fixturesPath, firstYamlFile, secondYamlFile, resultYamlFile);
+  const expectedContent = fs.readFileSync(expectedYamlFilePath, 'utf-8');
 
   test('Files format should be YAML and both file formats should be equal', () => {
-    expect(firstFileFormat).toBe('.yaml');
-    expect(secondFileFormat).toBe('.yaml');
-    expect(firstFileFormat === secondFileFormat).toBeTruthy();
+    expect(firstYamlFileFormat).toBe('.yaml');
+    expect(secondYamlFileFormat).toBe('.yaml');
+    expect(firstYamlFileFormat === secondYamlFileFormat).toBeTruthy();
   });
 
   test('Should return difference between two YAML files', () => {
-    expect(genDiff(firstFilePath, secondFilePath)).toBe(expectedContent);
+    expect(genDiff(firstYamlFilePath, secondYamlFilePath)).toBe(expectedContent);
   });
 
   test('Result file shoud be truthy and defined', () => {
-    expect(genDiff(firstFilePath, secondFilePath)).toBeTruthy();
-    expect(genDiff(firstFilePath, secondFilePath)).toBeDefined();
+    expect(genDiff(firstYamlFilePath, secondYamlFilePath)).toBeTruthy();
+    expect(genDiff(firstYamlFilePath, secondYamlFilePath)).toBeDefined();
   });
 });
 //
