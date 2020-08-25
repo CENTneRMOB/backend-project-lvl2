309a1ae8018e1f6847ec3598d96dfde42eba4e9f
Boris P <pustovetov.boris@yandex.ru>
2020-08-26 02:30:03 +0300

    step 5

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,7 +1,5 @@
-import fs from 'fs';
 import _ from 'lodash';
-import path from 'path';
-import getParser from './parsers.js';
+import getContent from './parsers.js';
 
 const ADD = 'add';
 const REMOVE = 'remove';
@@ -9,12 +7,7 @@ const EQUAL = 'equal';
 const CHANGED = 'changed';
 
 const genDiff = (filePath1, filePath2) => {
-  const content1 = fs.readFileSync(filePath1, 'utf-8');
-  const content2 = fs.readFileSync(filePath2, 'utf-8');
-  const fileExtname = path.extname(filePath1);
-  const parse = getParser(fileExtname);
-  const obj1 = parse(content1);
-  const obj2 = parse(content2);
+  const [obj1, obj2] = getContent(filePath1, filePath2);
 
   const keysOfObj1 = Object.keys(obj1);
   const keysOfObj2 = Object.keys(obj2);
--- src/parsers.js
+++ src/parsers.js
@@ -1,5 +1,15 @@
 import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
 
 const formatWithParsers = { '.json': JSON.parse, '.yaml': yaml.safeLoad };
 
-export default (extension) => formatWithParsers[extension];
+export default (filePath1, filePath2) => {
+  const getExtension = path.extname(filePath1);
+  const parse = formatWithParsers[getExtension];
+  const content1 = fs.readFileSync(filePath1, 'utf-8');
+  const content2 = fs.readFileSync(filePath2, 'utf-8');
+  const obj1 = parse(content1);
+  const obj2 = parse(content2);
+  return [obj1, obj2];
+};
