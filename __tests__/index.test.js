bc7e84b10ce9a0f94f7e0484fa295614c18d1a09
Boris P <pustovetov.boris@yandex.ru>
2020-08-27 03:04:50 +0300

    step 6, tests refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,25 @@
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../index.js';
-import factory from '../src/testingFactory.js';
 
 const fixturesPath = path.resolve('./__fixtures__');
 const resultFilePath = path.join(fixturesPath, 'result');
 const expectedContent = fs.readFileSync(resultFilePath, 'utf-8');
 
-describe.each([['file1.json', 'file2.json'], ['file1.yaml', 'file2.yaml'], ['file1.ini', 'file2.ini']])('Test block', (firstFile, secondFile) => {
-  const [firstFilePath, secondFilePath, firstFileFormat,
-    secondJsonFileFormat] = factory(fixturesPath, firstFile, secondFile);
+describe.each([['file1.json', 'file2.json', '.json'], ['file1.yaml', 'file2.yaml', '.yaml'],
+  ['file1.ini', 'file2.ini', '.ini']])('Test block', (firstFile, secondFile, format) => {
+  const firstFilePath = path.join(fixturesPath, firstFile);
+  const secondFilePath = path.join(fixturesPath, secondFile);
+  const firstFileFormat = path.extname(firstFilePath);
+  const secondFileFormat = path.extname(secondFilePath);
 
-  test('Files format should equal', () => {
-    expect(firstFileFormat === secondJsonFileFormat).toBeTruthy();
+  test(`Files format should be '${format}' and both file formats should be equal`, () => {
+    expect(firstFileFormat).toBe(`${format}`);
+    expect(secondFileFormat).toBe(`${format}`);
+    expect(firstFileFormat === secondFileFormat).toBeTruthy();
   });
 
-  test('Should return difference between two files', () => {
+  test(`Should return difference between two '${format}' files`, () => {
     expect(genDiff(firstFilePath, secondFilePath)).toBe(expectedContent);
   });
 
