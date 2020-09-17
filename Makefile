6f08bdf4dac1bc365fb026732d14fd9e5b5e9d2c
Boris P <pustovetov.boris@yandex.ru>
2020-09-17 03:15:23 +0300

    configuration and tests update

--- Makefile
+++ Makefile
@@ -1,17 +1,19 @@
-install:
-		npm install
+install: install-deps
 
 start:
-		node bin/gendiff.js
+	node bin/gendiff.js
+
+install-deps:
+	npm ci
 
 publish:
-		npm publish --dry-run
+	npm publish --dry-run
 
 lint:
-		npx eslint .
+	npx eslint .
 
 test:
-		npm test	
+	npm test	
 
 test-coverage:
-	    npm test -- --coverage --coverageProvider=v8
+	npm test -- --coverage --coverageProvider=v8
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,39 +2,32 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../index.js';
 
-const fixturesPath = path.resolve('./__fixtures__');
-const stylishResultFilePath = path.join(fixturesPath, 'stylish.result');
-const plainResultFilePath = path.join(fixturesPath, 'plain.result');
-const jsonResultFilePath = path.join(fixturesPath, 'json.result');
-const stylishExpectedContent = fs.readFileSync(stylishResultFilePath, 'utf-8');
-const plainExpectedContent = fs.readFileSync(plainResultFilePath, 'utf-8');
-const jsonExpectedContent = fs.readFileSync(jsonResultFilePath, 'utf-8');
-
-const beforeFileName = 'file1';
-const afterFileName = 'file2';
-
-const getFilePathAndExtension = (name, extension) => {
-  const filePath = path.join(fixturesPath, `${name}${extension}`);
-  const fileExtension = path.extname(filePath);
-  return [filePath, fileExtension];
+const getFilePath = (name, extension) => {
+  const dirname = path.resolve('./__fixtures__');
+  return path.join(dirname, `${name}.${extension}`);
 };
 
-describe.each([['.json'], ['.yaml'], ['.ini']])('Test block', (extension) => {
-  const [firstFilePath, firstFileExtension] = getFilePathAndExtension(beforeFileName, extension);
-  const [secondFilePath, secondFileExtension] = getFilePathAndExtension(afterFileName, extension);
+let stylishExpectedContent;
+let plainExpectedContent;
+let jsonExpectedContent;
 
-  test(`Files extensions should be '${extension}' and both files extensions should be equal`, () => {
-    expect(firstFileExtension).toBe(`${extension}`);
-    expect(secondFileExtension).toBe(`${extension}`);
-    expect(firstFileExtension === secondFileExtension).toBeTruthy();
-  });
-  test(`Should return difference between two ${extension} in stylish format`, () => {
+beforeAll(() => {
+  stylishExpectedContent = fs.readFileSync(getFilePath('stylish-result', 'txt'), 'utf-8');
+  plainExpectedContent = fs.readFileSync(getFilePath('plain-result', 'txt'), 'utf-8');
+  jsonExpectedContent = fs.readFileSync(getFilePath('json-result', 'txt'), 'utf-8');
+});
+
+describe.each([['json'], ['yaml'], ['ini']])('Testing each input file\'s format with each output format', (extension) => {
+  const firstFilePath = getFilePath('file1', extension);
+  const secondFilePath = getFilePath('file2', extension);
+
+  test(`Should return difference between two .${extension} in stylish format`, () => {
     expect(genDiff(firstFilePath, secondFilePath, 'stylish')).toBe(stylishExpectedContent);
   });
-  test(`Should return difference between two ${extension} in plain format`, () => {
+  test(`Should return difference between two .${extension} in plain format`, () => {
     expect(genDiff(firstFilePath, secondFilePath, 'plain')).toBe(plainExpectedContent);
   });
-  test(`Should return difference between two ${extension} in json format`, () => {
+  test(`Should return difference between two .${extension} in json format`, () => {
     expect(genDiff(firstFilePath, secondFilePath, 'json')).toBe(jsonExpectedContent);
   });
 });
