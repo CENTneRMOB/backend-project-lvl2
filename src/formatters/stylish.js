75962bb9a4e9bc150d706ec48c851271eccc919f
Boris P <pustovetov.boris@yandex.ru>
2020-10-27 15:08:08 +0300

    refactoring after 24.10.2020 review

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const getIndent = (space) => ' '.repeat(space);
+const getIndent = (spaceCount) => ' '.repeat(spaceCount);
 const indentStep = 4;
 
 const stringify = (value, depth) => {
@@ -8,40 +8,39 @@ const stringify = (value, depth) => {
     return value;
   }
   const innerDepth = depth + indentStep;
-  const valueEntries = Object.entries(value)
-    .map(([key, val]) => `${getIndent(innerDepth)}    ${key}: ${stringify(val, innerDepth)}`);
+  const lines = Object.entries(value)
+    .map(([key, val]) => `${getIndent(innerDepth)}${key}: ${stringify(val, innerDepth)}`);
 
   return [
     '{',
-    ...valueEntries,
-    `${getIndent(innerDepth)}}`,
+    ...lines,
+    `${getIndent(depth)}}`,
   ].join('\n');
 };
 
 const mapping = {
-  add: (node, depth) => `${getIndent(depth)}  + ${node.key}: ${stringify(node.value, depth)}`,
-  remove: (node, depth) => `${getIndent(depth)}  - ${node.key}: ${stringify(node.value, depth)}`,
-  equal: (node, depth) => `${getIndent(depth)}    ${node.key}: ${stringify(node.value, depth)}`,
+  add: (node, depth) => `${getIndent(depth)}  + ${node.key}: ${stringify(node.value, depth + indentStep)}`,
+  remove: (node, depth) => `${getIndent(depth)}  - ${node.key}: ${stringify(node.value, depth + indentStep)}`,
+  equal: (node, depth) => `${getIndent(depth)}    ${node.key}: ${stringify(node.value, depth + indentStep)}`,
   changed: (node, depth) => [
-    `${getIndent(depth)}  - ${node.key}: ${stringify(node.value1, depth)}`,
-    `${getIndent(depth)}  + ${node.key}: ${stringify(node.value2, depth)}`,
+    `${getIndent(depth)}  - ${node.key}: ${stringify(node.value1, depth + indentStep)}`,
+    `${getIndent(depth)}  + ${node.key}: ${stringify(node.value2, depth + indentStep)}`,
   ],
-  nested: (item, nodeDepth, iter) => [
-    `${getIndent(nodeDepth + indentStep)}${item.key}: {`,
-    ...iter(item.children, nodeDepth + indentStep),
-    `${getIndent(nodeDepth + indentStep)}}`,
-  ],
+  nested: (item, depth, iter) => `${getIndent(depth)}    ${item.key}: ${iter(item.children, depth + indentStep)}`,
 };
 
 const stylish = (diff) => {
-  const iter = (nodeItems, nodeDepth) => nodeItems
-    .flatMap((item) => mapping[item.type](item, nodeDepth, iter));
+  const iter = (nodes, depth) => {
+    const lines = nodes.flatMap((node) => mapping[node.type](node, depth, iter));
 
-  return [
-    '{',
-    ...iter(diff, 0),
-    '}',
-  ].join('\n');
+    return [
+      '{',
+      ...lines,
+      `${getIndent(depth)}}`,
+    ].join('\n');
+  };
+
+  return iter(diff, 0);
 };
 
 export default stylish;
